69348e1dc0c3969ea47e0c9fe4f1ddb2e6a48924
Nassahel Elias <79935315+nassahel@users.noreply.github.com>
2023-11-18 17:30:18 -0300

    primer commit

---
+++ src/main/java/com/trabajoFinalJava/argPrograma/App.java
@@ -0,0 +1,13 @@
+package com.trabajoFinalJava.argPrograma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
